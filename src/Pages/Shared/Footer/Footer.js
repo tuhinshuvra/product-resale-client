85c225af88cbf298d51ba9b8a9b59357667929e3
Tuhin Shuvra <tuhinshuvra18@gmail.com>
2022-11-24 15:44:04 +0600

    added footer

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,11 +1,37 @@
 import React from 'react';
-import './Footer.css';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
-        <div>
-            <h2>This is Footer Page</h2>
-        </div>
+        <footer className='bg-neutral'>
+            <div className="footer p-10 bg-neutral text-neutral-content">
+                <div>
+                    <span className="footer-title">Services</span>
+                    <Link to="" className="link link-hover">Branding</Link>
+                    <Link to="" className="link link-hover">Design</Link>
+                    <Link to="" className="link link-hover">Marketing</Link>
+                    <Link to="" className="link link-hover">Advertisement</Link>
+                </div>
+                <div>
+                    <span className="footer-title">Company</span>
+                    <Link to="" className="link link-hover">About us</Link>
+                    <Link to="" className="link link-hover">Contact</Link>
+                    <Link to="" className="link link-hover">Jobs</Link>
+                    <Link to="" className="link link-hover">Press kit</Link>
+                </div>
+                <div>
+                    <span className="footer-title">Legal</span>
+                    <Link to="" className="link link-hover">Terms of use</Link>
+                    <Link to="" className="link link-hover">Privacy policy</Link>
+                    <Link to="" className="link link-hover">Cookie policy</Link>
+                </div>
+            </div>
+            <div className="footer footer-center p-4 text-white">
+                <div>
+                    <p>Copyright © 2022 - All right reserved by Tuhin Shuvra</p>
+                </div>
+            </div>
+        </footer>
     );
 };
 
