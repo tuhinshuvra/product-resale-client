aa98dd7470d12990c9f74bf1247359f54d0a4fc4
Tuhin Shuvra <tuhinshuvra18@gmail.com>
2022-11-25 09:02:46 +0600

    updated carousol

--- src/Pages/Home/Carousol/Carousol.js
+++ src/Pages/Home/Carousol/Carousol.js
@@ -1,45 +1,67 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import laptop1 from '../../../assets/laptops/laptop-1.jpg';
-import laptop2 from '../../../assets/laptops/laptop-3.jpg';
-import laptop3 from '../../../assets/laptops/laptop-5.jpg';
-import laptop4 from '../../../assets/laptops/laptop-9.jpg';
+import laptop2 from '../../../assets/laptops/laptop-2.jpg';
+import laptop3 from '../../../assets/laptops/laptop-3.jpg';
+import laptop4 from '../../../assets/laptops/laptop-4.jpg';
+import laptop5 from '../../../assets/laptops/laptop-5.jpg';
+import laptop6 from '../../../assets/laptops/laptop-6.jpg';
+import CarousolItem from './CarousolItem';
 
+const carousolData = [
+    {
+        image: laptop1,
+        prev: 6,
+        id: 1,
+        next: 2,
+    },
+    {
+        image: laptop2,
+        prev: 1,
+        id: 2,
+        next: 3,
+    },
+    {
+        image: laptop3,
+        prev: 2,
+        id: 3,
+        next: 4,
+    },
+    {
+        image: laptop4,
+        prev: 3,
+        id: 4,
+        next: 5,
+    },
+    {
+        image: laptop5,
+        prev: 4,
+        id: 5,
+        next: 6,
+    },
+    {
+        image: laptop6,
+        prev: 5,
+        id: 6,
+        next: 1,
+    },
+]
 
 const Carousol = () => {
     return (
 
-        <div>
-            <div className="carousel w-full">
-                <div id="slide1" className="carousel-item relative w-full">
-                    <img src="https://placeimg.com/800/200/arch" alt='' className="w-full" />
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <Link href="#slide4" className="btn btn-circle">❮</Link>
-                        <Link href="#slide2" className="btn btn-circle">❯</Link>
-                    </div>
-                </div>
-                <div id="slide2" className="carousel-item relative w-full">
-                    <img src="https://placeimg.com/800/200/arch" alt='' className="w-full" />
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <Link href="#slide1" className="btn btn-circle">❮</Link>
-                        <Link href="#slide3" className="btn btn-circle">❯</Link>
-                    </div>
-                </div>
-                <div id="slide3" className="carousel-item relative w-full">
-                    <img src="https://placeimg.com/800/200/arch" alt='' className="w-full" />
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <Link href="#slide2" className="btn btn-circle">❮</Link>
-                        <Link href="#slide4" className="btn btn-circle">❯</Link>
-                    </div>
-                </div>
-                <div id="slide4" className="carousel-item relative w-full">
-                    <img src="https://placeimg.com/800/200/arch" alt='' className="w-full" />
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <Link href="#slide3" className="btn btn-circle">❮</Link>
-                        <Link href="#slide1" className="btn btn-circle">❯</Link>
-                    </div>
-                </div>
-            </div>
+        <div className=' carousel w-full'>
+            {
+                carousolData.map(slide =>
+                    <CarousolItem
+                        key={slide.id}
+                        slide={slide}
+                    ></CarousolItem>
+                )
+            }
+
+
+
         </div>
     );
 };
